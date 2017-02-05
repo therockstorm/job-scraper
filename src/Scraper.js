71d6e26fbdb26f587d1911f03ba97dea4a7ed481
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2017-02-04 20:45:38 -0800

    Add doc

--- src/Scraper.js
+++ src/Scraper.js
@@ -7,10 +7,6 @@ const scraperError = (remainingIds, failedIds) => error(`Failed to scrape jobs.
 remainingIds=${Array.from(remainingIds).toString()}
 failedIds=${failedIds.toString()}`);
 
-// Questions:
-//  Don't hard-code PAGE_SIZE
-//  Can I use libs to parse the html?
-
 const PAGE_SIZE = 10;
 const WHITESPACE_RE = new RegExp(/\s\s+/, 'g');
 const JOBS_RE = new RegExp('^/jobs/([0-9]+)$', 'i');
@@ -22,25 +18,33 @@ export default class Scraper {
     this.jobIds = new Set();
   }
 
+  /**
+   * Gets ids to scrape, scapes them, and either logs errors or completes the challenge.
+   */
   run() {
     this.client
       .getJobIds()
       .then((ids) => {
         ids.forEach(id => this.jobIds.add(id));
-        const jobPromises = this.scrapeJobs(ids.length / PAGE_SIZE);
-        Promise.all(jobPromises)
+        Promise.all(this.scrapeJobs(ids.length / PAGE_SIZE))
           .then(() => {
-            const failures = this.jobIds.size || this.failedIds.length;
-            if (failures) scraperError(this.jobIds, this.failedIds);
+            const errors = this.jobIds.size || this.failedIds.length;
+            if (errors) scraperError(this.jobIds, this.failedIds);
             else this.client.completeChallenge().then(res => log(res));
           });
       });
   }
 
+  /**
+   * For each page of jobs, get the page, scrape it for jobs, and post each batch to Challenge API.
+   * If the batch succeeds, remove the ides from the jobIds Set.
+   * @param {Number} pages
+   * @return {Promise[]} promises
+   */
   scrapeJobs(pages) {
     return [...Array(pages)].map((_, i) => new Promise((resolve) => {
       const page = i + 1;
-      this.faultTolerantGetJobs(page)
+      this.getJobs(page)
         .then(html => this.scrapePage(html))
         .then(jobs => this.client.createJobs(jobs, page)
           .then((res) => {
@@ -50,7 +54,12 @@ export default class Scraper {
     }));
   }
 
-  faultTolerantGetJobs(page) {
+  /**
+   * Get jobs page retrying up to 5 times on error.
+   * @param {Number} pages
+   * @return {Promise<string>} html
+   */
+  getJobs(page) {
     return new Promise((resolve) => {
       const op = retry.operation({ retries: 5 });
       op.attempt(() => {
@@ -60,6 +69,11 @@ export default class Scraper {
     });
   }
 
+  /**
+   * Scrape each matching 'a' tags on a jobs page.
+   * @param {string} html
+   * @return {Object} jobs
+   */
   scrapePage(html) {
     return new Promise((resolve) => {
       const jobs = [];
@@ -76,6 +90,13 @@ export default class Scraper {
     });
   }
 
+  /**
+   * Scrape an element. Add failures to failedIds.
+   * @param {Object} $
+   * @param {Object} elem
+   * @param {Number} jobId
+   * @return {Object} job
+   */
   scrapeJob($, elem, jobId) {
     const job = {
       employer_job_id: Scraper.scrapeStr($, '.job-number', elem),
@@ -91,6 +112,15 @@ export default class Scraper {
     return false;
   }
 
+  /**
+   * Scrape and normalize a string.
+   * @param {Object} $
+   * @param {string} cls
+   * @param {Number} parent
+   * @param {string} sep
+   * @param {Number} idx
+   * @return {string} str
+   */
   static scrapeStr($, cls, parent, sep = ':', idx = 1) {
     const text = $(cls, parent).text();
     if (text.indexOf(sep) === -1) return '';
